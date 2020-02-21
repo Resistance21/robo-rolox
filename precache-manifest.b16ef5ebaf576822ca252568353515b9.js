15764bc2441bdce5cec38ad1df7965b651ce3a95
brendan hales <brendan_hales@hotmail.com>
2020-02-22 02:06:34 +0800

    Updates

---
+++ precache-manifest.b16ef5ebaf576822ca252568353515b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73c4de549291c28acc5b12cbf2f182f",
+    "url": "/robo-rolox/index.html"
+  },
+  {
+    "revision": "daf5c2abdcfc9162b968",
+    "url": "/robo-rolox/static/css/main.aae2d587.chunk.css"
+  },
+  {
+    "revision": "b1359da5e56a9eadd75a",
+    "url": "/robo-rolox/static/js/2.909ccc88.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robo-rolox/static/js/2.909ccc88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daf5c2abdcfc9162b968",
+    "url": "/robo-rolox/static/js/main.979b5b2e.chunk.js"
+  },
+  {
+    "revision": "78361ab3134821eafb67",
+    "url": "/robo-rolox/static/js/runtime-main.93cc7bc2.js"
+  }
+]);
